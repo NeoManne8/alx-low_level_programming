0ca3fce8e98b66219ac5ee4da742a47f748d540f
NeoManne8 <nmanne8@gmail.com>
2023-07-22 12:33:26 -0700

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet-x10 - prints alphabet in lowercase 10 times
- * Return: always 0 (success)
+ * print_alphabet-x10 - prints alphabet in lowercase 10x 
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
